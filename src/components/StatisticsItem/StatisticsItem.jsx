a838d358fb15a3249a43625bd166c68d131bbc48
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-08-10 12:16:16 +0300

    Add task_2

--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,3 +1,15 @@
-export const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import { IconContext } from 'react-icons';
+
+import styled from './Statistics.module.css';
+
+export const StatisticsItem = ({ title, total, icon }) => {
+  return (
+    <li className={styled.item}>
+      <IconContext.Provider value={{ size: 28, color: '#2096F3' }}>
+        {icon}
+      </IconContext.Provider>
+      <span className={styled.counter}>{total}</span>
+      <p className={styled.text}>{title}</p>
+    </li>
+  );
 };
