07bf67b73bcf529d575ac3e70e922b9672d7a66a
Serhii Reshetniak <reshet@lozovaya.org.ua>
2024-08-10 10:53:00 +0300

    Start files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Section,
+  Container,
+  BlogCard,
+  Heading,
+  Statistics,
+  CryptoHistory,
+  ForbesList,
+} from 'components';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Section>
+      <Container>
+        <Heading title="Task 1 Blog Card" bottom />
+        <BlogCard />
 
-export default App
+        <Heading title="Task 2 Statistics" top bottom />
+        <Statistics title="Main Statistics" />
+
+        <Heading title="Task 3 Forbes list" top bottom />
+        <ForbesList />
+
+        <Heading title="Task 4 Crypto history" top bottom />
+        <CryptoHistory />
+      </Container>
+    </Section>
+  );
+};
