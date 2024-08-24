b0c794cb42c724f121e881204912352c39ae2a00
Oleh Mogylevsky <159718895+mogilevs@users.noreply.github.com>
2024-08-24 12:57:19 +0300

    do task1

--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,10 +1,35 @@
-// import { getPhotos } from 'apiService/photos';
-import { Text } from 'components';
+import { getPhotos } from 'apiService/photos';
+import { Form, PhotosGallery, Text } from 'components';
+import { useEffect, useState } from 'react';
 
 export const Photos = () => {
+  const [value, setValue] = useState('');
+  const [page, setPage] = useState(1);
+  const [photos, setPhotos] = useState([]);
+
+  function onSubmit(query) {
+    setValue(query);
+  }
+
+  useEffect(() => {
+    if (!value) return;
+
+    async function getData() {
+      try {
+        const { photos } = await getPhotos(value, page);
+        setPhotos(photos);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    getData();
+  }, [value, page]);
+
   return (
     <>
       <Text textAlign="center">Let`s begin search 🔎</Text>
+      <Form onSubmit={onSubmit} />
+      <PhotosGallery photos={photos} />
     </>
   );
 };
