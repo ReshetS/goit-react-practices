14203a77b72f5aa6d422e2a168a617b83a9d6bf3
Hanna Lakus <annalakus@ukr.net>
2024-10-05 09:37:46 +0100

    first task

--- src/redux/commentApi.js
+++ src/redux/commentApi.js
@@ -1,15 +1,19 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_ENDPOINT = '/comments';
-const BASE_URL = '';
+const API_ENDPOINT = "/comments";
+const BASE_URL = "https://66d4c2b95b34bcb9ab3f682c.mockapi.io/";
 
 export const commentApi = createApi({
-  reducerPath: 'comments',
+  reducerPath: "comments",
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
   }),
-  tagTypes: ['Comments'],
-  endpoints: (builder) => ({}),
+  tagTypes: ["Comments"],
+  endpoints: (builder) => ({
+    getComments: builder.query({
+      query: () => ({ url: API_ENDPOINT }),
+    }),
+  }),
 });
 
-export const {} = commentApi;
+export const { useGetCommentsQuery } = commentApi;
